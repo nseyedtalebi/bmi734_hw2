4be20d754c48b3ff4716642f6b9413373ac9955d
Nima Seyedtalebi <nseyedtalebi@gmail.com>
2019-02-23 21:52:12 -0500

    Realized that extra registration step for rotation was performing worse than just searching for the rotation angle by brute force

--- regist.py
+++ regist.py
@@ -22,32 +22,31 @@ best_y = None
 for sig in sigmas:
 	for x in xs:
 		for y in ys:
-			orig = gaussian(io.imread('BrainProtonDensitySlice.png'),sigma=sig)
-			moved = gaussian(io.imread('BrainProtonDensitySliceR10X13Y17.png'),sigma=sig)
-			sft,idc,idk = register_translation(pad(orig,20,'edge'),moved)
-			unmoved = warp(moved,EuclideanTransform(translation=[-sft[1],-sft[0]]),output_shape=(257,221,),mode='constant',cval=0)
-			cropped = unmoved[20:-20,20:-20]
-			io.imsave('unmoved_cropped.png',cropped)
-			orig_tr = logpolar_fancy(np.fft.fftn(orig),x,y)#91,108
-			cropped_tr = logpolar_fancy(np.fft.fftn(cropped),x,y)#91,108
-			shifted_lpt,error_lpt,phased_lpt = register_translation(orig_tr,cropped_tr,space='fourier')
-			rot_rads = math.atan2(shifted_lpt[0],shifted_lpt[1])
-			rot_degs = math.degrees(rot_rads)
-			rotated = rotate(cropped,rot_degs)
-			io.imsave('rotated.png',rotated)
-			#shifts,error,phasediff = register_translation(orig,rotated,1000)
-			#shifted = warp(rotated,EuclideanTransform(translation=shifts))
-			#shifted = (shifted*255).astype('uint8')
-			mssim,diff = compare_ssim(orig,rotated,full=True)
-			if mssim > best_mssim:
-				best_mssim=mssim
-				best_x = x
-				best_y = y
-				best_sig = sig
-			print(f'Gaussian sigma:{sig}\nRotation (degs):{rot_degs}\nShift:{sft}\nMSSIM:{mssim}')
-			#io.imsave('shifted.png',shifted)
-			io.imsave('diff.png',diff)
-print(f'best mssim:{best_mssim}\nbest_x{best_x} best_y{best_y}\nbest_sig{best_sig}')
+			for rot in [8.2]:
+				orig = gaussian(io.imread('BrainProtonDensitySlice.png'),sigma=sig)
+				moved = gaussian(io.imread('BrainProtonDensitySliceR10X13Y17.png'),sigma=sig)
+				sft,idc,idk = register_translation(pad(orig,20,'edge'),moved)
+				unmoved = warp(moved,EuclideanTransform(translation=[-sft[1],-sft[0]]),output_shape=(257,221,),mode='constant',cval=0)
+				cropped = unmoved[20:-20,20:-20]
+				io.imsave('unmoved_cropped.png',cropped)
+				#orig_tr = logpolar_fancy(np.fft.fftn(orig),x,y)#91,108
+				#cropped_tr = logpolar_fancy(np.fft.fftn(cropped),x,y)#91,108
+				#shifted_lpt,error_lpt,phased_lpt = register_translation(orig_tr,cropped_tr,space='fourier')
+				#rot_rads = math.atan2(shifted_lpt[0],shifted_lpt[1])
+				#rot_degs = math.degrees(rot_rads)
+				rotated = rotate(cropped,rot)
+				io.imsave('rotated.png',rotated)
+				#shifts,error,phasediff = register_translation(orig,rotated,1000)
+				#shifted = warp(rotated,EuclideanTransform(translation=shifts))
+				#shifted = (shifted*255).astype('uint8')
+				mssim,diff = compare_ssim(orig,rotated,full=True)
+				if mssim > best_mssim:
+					best_mssim=mssim
+					best_rot = rot
+				print(f'Rotation (degs):{rot}\nMSSIM:{mssim}')
+				#io.imsave('shifted.png',shifted)
+				io.imsave('diff.png',diff)
+			print(f'best mssim:{best_mssim}\nbest_rot:{best_rot}')
 '''Gaussian sigma:4.735
 Rotation (degs):0.0
 Shift:[-16. -12.]
@@ -55,3 +54,8 @@ MSSIM:0.9060310863982878
 best mssim:0.9060310863982878
 best_x90.5 best_y107.5
 best_sig4.735'''
+###Got better results just varying rotation param
+'''Rotation (degs):8.2
+MSSIM:0.9608167494650862
+best mssim:0.9608167494650862
+best_rot:8.2'''
